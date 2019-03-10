62bc58279ccb4f3a8b2ed37ac36cdaafd0e12bdf
Md. Manjurul Hussain Shourov <mmhs013@gmail.com>
2019-03-10 18:00:48 +0600

    add function list in __init__

---
+++ pymannkendall/__init__.py
@@ -0,0 +1,4 @@
+from pymannkendall import sens_slope, seasonal_sens_slope, original_test, hamed_rao_modification_test, yue_wang_modification_test, pre_whitening_modification_test, trend_free_pre_whitening_modification_test, multivariate_test, seasonal_test, regional_test, correlated_multivariate_test, correlated_seasonal_test, partial_test 
+
+__version__ = 0.1a
+__all__ = [sens_slope, seasonal_sens_slope, original_test, hamed_rao_modification_test, yue_wang_modification_test, pre_whitening_modification_test, trend_free_pre_whitening_modification_test, multivariate_test, seasonal_test, regional_test, correlated_multivariate_test, correlated_seasonal_test, partial_test ]
